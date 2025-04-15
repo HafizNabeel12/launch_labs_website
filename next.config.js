01fd1c8c9eff00a55d1498020fd2d1c1dec4089b
HafizNabeel12 <138764369+HafizNabeel12@users.noreply.github.com>
2025-04-15 14:38:14 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains:["agency.teamrabbil.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
   },
 };
 
