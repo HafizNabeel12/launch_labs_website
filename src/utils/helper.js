cff584c2adbf6dd020523427b91d89b9b601549f
HafizNabeel12 <138764369+HafizNabeel12@users.noreply.github.com>
2025-09-15 14:28:08 +0500

    updated

--- src/utils/helper.js
+++
@@ -1,6 +0,0 @@
-export const parseDate = (createdData) => {
-    const date = new Date(createdData);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
-    return formattedDate
-}
